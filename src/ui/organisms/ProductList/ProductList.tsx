de3b9f44250c74e4afde5cf47a5fc6640803fbb8
RafalOpacki <rafalopacki@gmail.com>
2024-02-07 11:25:15 +0100

    task1-ui improvements

--- src/ui/organisms/ProductList/ProductList.tsx
+++ src/ui/organisms/ProductList/ProductList.tsx
@@ -3,8 +3,11 @@ import { products } from "@/ui/organisms/ProductList/mock";
 
 export const ProductList = () => {
 	return (
-		<section>
-			<ul data-testid="products-list" className="flex flex-row flex-wrap gap-2">
+		<section className="mx-auto max-w-2xl px-8 py-8 sm:px-6 lg:max-w-7xl">
+			<ul
+				data-testid="products-list"
+				className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"
+			>
 				{products.map((product) => {
 					return <ProductListItem key={product.id} product={product} />;
 				})}
--- src/ui/organisms/ProductList/mock.ts
+++ src/ui/organisms/ProductList/mock.ts
@@ -45,15 +45,4 @@ export const products: Product[] = [
 			alt: "Denim Jeans",
 		},
 	},
-	{
-		id: "cj3tr1r3q0000k3szspmh7twn",
-		name: "Red Dress",
-		category: "Ubrania",
-		description: "Elegant red dress for special occasions.",
-		price: 65,
-		img: {
-			src: "https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Ft-shirt-1.png%3Fv%3D1689798965&w=1920&q=75",
-			alt: "Red Dress",
-		},
-	},
 ];
